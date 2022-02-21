7c3a77b69450eb386a20d09b86ee9f2ccdb80dbb
RenierC <reniercuervo@gmail.com>
2022-02-20 20:16:40 -0400

    rolls back to previous staging server config

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,22 +1,22 @@
 import getConfig from "../config";
 import { NODE_ENV } from "./environment";
 
-// TODO: Here you can set different endpoints for your backend
+// TODO: Here you can set different endpoints for the backend
 const getBaseUrl = () => {
   if(NODE_ENV == "development") {
     console.log("development detected");
     return "http://localhost:3000";
   }
 
-  // if(NODE_ENV == "production") {
-  //   console.log("production detected");
-  //   return "your.production.server.here.xyz";
-  // }
+  if(NODE_ENV == "production") {
+    console.log("production detected");
+    return "your.staging.server.here.xyz";
+  }
   
-  // if(NODE_ENV == "staging") {
-  //   console.log("staging detected");
-  //   return "your.staging.server.here.xyz";
-  // }
+  if(NODE_ENV == "staging") {
+    console.log("staging detected");
+    return "eventcred-backend.herokuapp.com";
+  }
 }
 
 export const BASE_URL = getBaseUrl();
